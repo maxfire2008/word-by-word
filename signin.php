2362833d04ffb6f7c37fb969738cd6c81af2a5ef
maxrichardburgess <22722054+maxfire2008@users.noreply.github.com>
2021-08-19 19:20:10 +1000

    Update signin.php

--- signin.php
+++ signin.php
@@ -4,7 +4,7 @@ require 'conn.php';
 
 ob_start();
 
-header("HTTP/1.1 204 NO CONTENT");
+//header("HTTP/1.1 204 NO CONTENT");
 
 header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
 header("Pragma: no-cache"); // HTTP 1.0.
