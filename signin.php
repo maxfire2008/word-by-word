2317a74161555feea0400b3e6fdd6df64988f35e
maxrichardburgess <22722054+maxfire2008@users.noreply.github.com>
2021-08-19 19:32:59 +1000

    Update signin.php

--- signin.php
+++ signin.php
@@ -16,11 +16,11 @@ if (isset($_POST['userToken'])) {
 	$sql = "SELECT * FROM `users` WHERE '2c87f27f3f934cda8c6682deec079bea'";
 	$result = $mysqli->query($sql);
 	$resultArray = $result->fetch_assoc();
-	setcookie("user_token",$_POST['userToken']);
-	echo(implode(" ",$resultArray));
-	/*foreach ($resultArray as item) {
-		echo $row;
-	}*/
+	foreach ($resultArray as $row) {
+		if ($row[2] == $_POST['userToken']) {
+			setcookie("user_token",$_POST['userToken']);
+		}
+	}
 }
 
 ?>
