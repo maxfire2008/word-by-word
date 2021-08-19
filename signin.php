d7c848f68bc1002fd77c0f47841907c27fe17b54
maxrichardburgess <22722054+maxfire2008@users.noreply.github.com>
2021-08-19 19:14:21 +1000

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'conn.php';
+
 ob_start();
 
 header("HTTP/1.1 204 NO CONTENT");
@@ -11,7 +13,10 @@ header("Expires: 0"); // Proxies.
 ob_end_flush(); //now the headers are sent
 
 if (isset($_POST['userToken'])) {
+	$sql = "SELECT * FROM `users` WHERE '2c87f27f3f934cda8c6682deec079bea'";
+	$result = $conn->query($sql);
 	setcookie("user_token",$_POST['userToken']);
+	echo $result;
 }
 
 ?>
