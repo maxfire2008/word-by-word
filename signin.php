2e4e37ed1691024bd1a29be04f5a161c354d33ca
maxrichardburgess <22722054+maxfire2008@users.noreply.github.com>
2021-08-19 19:34:32 +1000

    Update signin.php

--- signin.php
+++ signin.php
@@ -2,15 +2,7 @@
 
 require 'conn.php';
 
-ob_start();
-
-header("205 Reset Content");
-
-header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
-header("Pragma: no-cache"); // HTTP 1.0.
-header("Expires: 0"); // Proxies.
-
-ob_end_flush(); //now the headers are sent
+http_response_code(205);
 
 if (isset($_POST['userToken'])) {
 	$sql = "SELECT * FROM `users` WHERE '2c87f27f3f934cda8c6682deec079bea'";
