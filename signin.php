c06dcb87061a7b16f2918e93f2ad8fe01fd165ad
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2021-08-20 13:12:49 +1000

    Update signin.php

--- signin.php
+++ signin.php
@@ -5,10 +5,10 @@ require 'conn.php';
 //header("HTTP/1.1 205 REFRESH CONTENT");
 
 if (isset($_POST['userToken'])) {
-	$sql = "SELECT `user_token` FROM `users` WHERE `user_token` = '".mysqli_real_escape_string($_POST['userToken'])."';";
+	$user_token = mysqli_real_escape_string($mysqli,$_POST['userToken']);
+	$sql = "SELECT `user_token` FROM `users` WHERE `user_token` = '".$user_token."';";
 	$result = $mysqli->query($sql);
 	$rowcount = $result->num_rows;
-	echo mysqli_real_escape_string($_POST['userToken']);echo "\n";
 	if ($rowcount) {
 		setcookie("user_token",$_POST['userToken']);
 	}
