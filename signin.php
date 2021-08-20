740f15d04d6a7ccefd2a2c79082bab6ec89b4cba
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2021-08-20 13:07:25 +1000

    Update signin.php

--- signin.php
+++ signin.php
@@ -5,7 +5,7 @@ require 'conn.php';
 //header("HTTP/1.1 205 REFRESH CONTENT");
 
 if (isset($_POST['userToken'])) {
-	$sql = "SELECT `user_token` FROM `users` WHERE `user_token` = '".mysqli_real_escape_string($_POST['userToken'])."'";
+	$sql = "SELECT `user_token` FROM `users` WHERE `user_token` = '".mysqli_real_escape_string($_POST['userToken'])."';";
 	$result = $mysqli->query($sql);
 	$rowcount = $result->num_rows;
 	echo $sql;
