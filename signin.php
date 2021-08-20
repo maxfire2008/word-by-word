dafdc7fecc8a54345b44f955baee67dd62dc2307
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2021-08-20 13:05:53 +1000

    Update signin.php

--- signin.php
+++ signin.php
@@ -2,7 +2,7 @@
 
 require 'conn.php';
 
-header("HTTP/1.1 205 REFRESH CONTENT");
+//header("HTTP/1.1 205 REFRESH CONTENT");
 
 if (isset($_POST['userToken'])) {
 	$sql = "SELECT `user_token` FROM `users` WHERE `user_token` = '".mysqli_real_escape_string($_POST['userToken'])."'";
